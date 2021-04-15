c2f861ae3a5df4cc4f3a1265f61adee543e640c8
tombeck <t@ocupop.com>
2021-04-14 23:49:38 -0700

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,28 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const BlogIndex = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="All posts" />
+      <h1>Test</h1>
+    </Layout>
+  )
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
